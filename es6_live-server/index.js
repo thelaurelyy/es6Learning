a949b03ee2b22cef78e6db744db7348d05e491cd
thelaurelyy <3246997955@qq.com>
2020-02-11 17:37:10 +0800

    es6中数组和函数补漏

--- es6_live-server/index.js
+++ es6_live-server/index.js
@@ -87,3 +87,81 @@ console.log(tempArr.next().value);
 
 
 
+console.log('箭头函数及拓展========================');
+function add(a, b = 3) {
+    // 'use strict';
+    if(a===0) {
+        throw new Error('A is Error')
+    }
+    console.log(a+b)
+}
+
+add(1, 2);
+add(1);
+console.log('add函数的入参长度：', add.length);
+
+var addFunc = (a, b) => a + b;
+var addFunc = (a, b) => { return a + b };
+var addFunc = (a, b) => {
+    console.log('jspang');
+    return a + b
+};
+console.log('箭头函数', addFunc(1, 2));
+
+
+
+
+console.log('函数及数组补漏========================');
+
+// 对象的函数解构
+let jsonStr = {
+    a: 'jspang',
+    b: '技术胖'
+};
+function func({a, b}) {
+    console.log('对象的函数解构', a, b);
+}
+func(jsonStr);
+
+
+// 数组的函数解构
+let numArr = ['jspang', '技术胖'];
+function func2(a, b) {
+    console.log('数组的函数解构', a, b);
+}
+func2(...numArr);
+
+
+// in 的用法
+let obj = {
+    a: 'jspang',
+    b: '技术胖'
+};
+console.log('in 的对象用法', 'a' in obj);
+
+// in 判断数组下标位置是否为空
+let arr6 = [,,,,,,,];
+console.log('不准确的判断方法', arr6.length);
+console.log('in 的数组用法：', 0 in arr6);
+
+
+// 数组遍历方法
+let arr7 = ['jspang', '技术胖'];
+
+    // forEach会自动忽略为空的数组元素
+    arr7.forEach((val, index) => {
+        console.log(index, val)
+    });
+
+    // filter
+    arr7.filter(x => console.log(x));
+
+    // some
+    arr7.some(x => console.log(x));
+
+    // map 在这里实现替换的作用
+    console.log(arr7.map(item=>'web'));
+
+// 数组转换为字符串
+console.log(arr7.join('|'));
+console.log(arr7.toString());
